019db873e14d6f552df9bb0ed5e37ce956ad2c7e
clive <98222803+AAClive@users.noreply.github.com>
2022-03-27 23:07:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ chrome_options=webdriver.ChromeOptions()
 chrome_options.binary_loction=os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-driver=webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")),options=chrome_options))
+driver=webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")),options=chrome_options)
 
 
 app=Flask(__name__)
