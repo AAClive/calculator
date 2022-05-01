08d2e7d7877092bbe6862fc0cefda991b5182d38
clive <98222803+AAClive@users.noreply.github.com>
2022-05-01 21:39:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ from flask import redirect
 from email.message import EmailMessage
 from selenium.webdriver.chrome.service import Service
 import socket
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.connect(("192.168.15.180",5050))
+app=Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def home():
+  if request.method=="POST":
+    variable = request.form.get("variable")
+    server.send(
+  render_template("home.html")
